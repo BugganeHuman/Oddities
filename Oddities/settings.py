bf88437cb67829b00534d30d271f815b6ac61945
BugganeHuman <markizdelakarabas@gmail.com>
2026-03-01 13:36:19 +0500

    moved to PostegeSQL

--- Oddities/settings.py
+++ Oddities/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'Oddities.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'oddities_db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT")
     }
 }
 
