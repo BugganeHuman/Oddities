6f0b391e9247d8d316ff1511c7ad01cb5207acb9
BugganeHuman <markizdelakarabas@gmail.com>
2026-03-03 16:36:12 +0500

    added sign up

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
-
+from .views import RegisterView
 urlpatterns = [
     path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh")
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register/", RegisterView.as_view(), name="register")
 ]
---
+++ users/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer
+from .models import User
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+
+
+"""
+Middleware → URL → Authentication → Permissions → View → Serializer → Database.
+"""
