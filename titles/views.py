4fb51ba1b2ba9a6139ab0dfb7027f964200d4308
BugganeHuman <markizdelakarabas@gmail.com>
2026-03-12 15:38:33 +0500

    added endpuints for sorted the  model Title

--- titles/views.py
+++ titles/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Title
 from .serializers import TitleSerializer
 from .services import get_id, get_cover, get_director, get_year_end
+from rest_framework.response import Response
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
@@ -46,4 +49,29 @@ class TitleViewSet(viewsets.ModelViewSet):
         if not cover:
             serializer.save(cover=get_cover(title, year_start)
         """
+
+
+@api_view(['GET'])
+def get_revisits(request):
+    titles = Title.objects.filter(status='RVS')
+    serializer = TitleSerializer(titles, many=True)
+    return Response (serializer.data)
+
+@api_view(['GET'])
+def order_by(request):
+    order = request.query_params.get('order_by')
+    results = None
+    if order == "rating":
+        results = Title.objects.order_by('rating')
+
+    elif order == "-rating":
+        results = Title.objects.order_by('-rating')
+
+    elif order == "-end_watch":
+        results = Title.objects.order_by('-end_watch')
+
+    elif order == "end_watch":
+        results = Title.objects.order_by('end_watch')
+
+    return Response(TitleSerializer(results, many=True).data)
 
