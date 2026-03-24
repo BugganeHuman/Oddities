88669dab927ff7cfbd35248f33d5af8ced5315c2
BugganeHuman <markizdelakarabas@gmail.com>
2026-03-25 01:58:26 +0500

    added register for tg_bot

--- tg_bot/bot_main.py
+++ tg_bot/bot_main.py
@@ -7,12 +7,19 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 import os
 import secrets
 import sqlite3
+from aiogram import F, types
 
-CONN = sqlite3.connect("/app/bot_db/tg_bot_db")
+
+
+CONN = sqlite3.connect("/app/bot_db/tg_bot_db.db")
 
 CURSOR = CONN.cursor()
 
-CURSOR.execute("CREATE TABLE IF NOT EXIST users (username TEXT, password TEXT)")
+CURSOR.execute("""CREATE TABLE IF NOT EXISTS users (
+               user_id INTEGER,
+               password TEXT NOT NULL,
+               created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+               );""")
 
 
 bot = Bot(token=os.getenv("BOT_TOKEN"))
@@ -21,47 +28,34 @@ dp = Dispatcher()
 
 router = Router()
 
-"""
-@dp.message()
-async def echo_handler(message: types.Message):
-    await message.answer(text=f"dick: {message.text}")
-"""
 
-from aiogram import F, types
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.filters import Command
-
-
-@dp.message(F.text == "fuck u")
-async def answer_to_hate(message: types.Message):
-    await message.answer("no, u " + message.text)
-
-
-#@dp.message(Command("start"))
-@dp.message(F.text == "s")
+@dp.message(Command("start"))
 async def start(message: types.Message):
-    # тут надо регать юзера
-    url = "http://web:8000/api/users/register"
+    url = "http://web:8000/api/users/register/"
 
     user_password = secrets.token_hex(16)
+    user_id = message.from_user.id
+
+    CURSOR.execute("INSERT INTO users (user_id, password) VALUES (?, ?)",
+            (user_id, user_password))
 
     data = {
         "username" : message.from_user.username,
-        "tg_id" : message.from_user.id,
-        "password" : user_password
+        "telegram_id" : user_id,
+        "password" : user_password,
+        "email" : f"user_{user_id}@oddities.com"
     }
 
     async with aiohttp.ClientSession() as session:
         async with session.post(url, json=data) as response:
-            pass
+            if response.status in [200, 201]:
+                await message.answer("Hello you have been register")
+            elif response.status in [400, 409]:
+                await message.answer("Welcome back")
+            else:
+                await message.answer(f"error in register ")
 
 
-    """
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            result = await response.json()
-            await message.answer(str(result))
-    """
 
 async def main():
     # Начинаем слушать сервера Телеграма (Polling)
