57b908403c0bad16ef6d972ff7abe21d33b8fab7
BugganeHuman <markizdelakarabas@gmail.com>
2026-04-21 00:18:12 +0500

    added API documentation + fixes

--- Oddities/settings.py
+++ Oddities/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'watchlist',
     'users',
     'criticism',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +116,14 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Oddities API',
+    'DESCRIPTION': 'Documentation for API Oddities',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False, }
 
 CELERY_BROKER_URL = 'redis://redis:6379'
 CELERY_RESULT_BACKEND = 'redis://redis:6379'
